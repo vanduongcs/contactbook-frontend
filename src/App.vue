8a0230e6861d1d918893a450f71bd9e2409b95a4
vanduongcs <ddvanworkmail@gmail.com>
2025-07-24 00:26:01 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+};
+</script>
+
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+}
+</style>
